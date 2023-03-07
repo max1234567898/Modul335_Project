f6260da22576ed9f68f000f44db7d2e6d1c137b9
Max.Ludwig <max.ludwig@lernende.bbw.ch>
2023-03-07 10:55:19 +0100

    fix: refactored API Service by Deleting unused usings and methods

--- app/src/main/java/ch/digitecgalaxus/modul_335_projekt/Service/ApiService.java
+++ app/src/main/java/ch/digitecgalaxus/modul_335_projekt/Service/ApiService.java
@@ -4,37 +4,15 @@ import android.app.Service;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
-import android.os.Binder;
 import android.os.IBinder;
-import android.widget.EditText;
-
+import androidx.annotation.Nullable;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import ch.digitecgalaxus.modul_335_projekt.WeatherActivity;
 
 public class ApiService extends Service {
     public static final String LongAndAti = "LongAndAti";
-
-    public List<Double> implementGeocoder(EditText inputFromField) throws IOException {
-        List<Double> resultList = new ArrayList<>();
-        Geocoder geocoder = new Geocoder(this);
-        List<Address> geocode = geocoder.getFromLocationName(String.valueOf(inputFromField), 1);
-
-        Double latitude = geocode.get(0).getLatitude();
-        Double longitude = geocode.get(0).getLongitude();
-
-        Intent broadcastIntent = new Intent();
-        broadcastIntent.setAction(WeatherActivity.LongAndAti);
-        broadcastIntent.putExtra("longitude", longitude);
-        broadcastIntent.putExtra("latitude", latitude);
-        sendBroadcast(broadcastIntent);
-
-        resultList.add(latitude);
-        resultList.add(longitude);
-        return resultList;
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -61,23 +39,9 @@ public class ApiService extends Service {
         return START_NOT_STICKY;
     }
 
-    // Binder given to clients
-    private final IBinder binder = new LocalBinder();
-    /**
-     * Class used for the client Binder.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with IPC.
-     */
-    public class LocalBinder extends Binder {
-        public ApiService getService() {
-            // Return this instance of LocalService so clients can call public methods
-            return ApiService.this;
-        }
-    }
-    public ApiService() {
-    }
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return binder;
+        return null;
     }
-
 }
