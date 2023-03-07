7fac01e4c95db13631a7e4c5e156d0f9081d8ed9
Max.Ludwig <max.ludwig@lernende.bbw.ch>
2023-03-07 10:57:25 +0100

    feat: Created output of Weather data fixed Layout

--- app/src/main/java/ch/digitecgalaxus/modul_335_projekt/WeatherActivity.java
+++ app/src/main/java/ch/digitecgalaxus/modul_335_projekt/WeatherActivity.java
@@ -1,26 +1,18 @@
 package ch.digitecgalaxus.modul_335_projekt;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
 import ch.digitecgalaxus.modul_335_projekt.Service.ApiService;
-import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -37,19 +29,44 @@ public class WeatherActivity extends AppCompatActivity {
 
     private Response response;
 
+    private String[] responseSplit;
+
+    private TextView cityName;
+    private TextView temperature;
+    private String input;
+
+    private JSONObject json;
+
+    private String result;
+
+    private String test = "\\{";
+
+
+
+
+
     private BroadcastReceiver longAndAtiReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
              longitude = intent.getDoubleExtra("longitude", 0);
              latitude = intent.getDoubleExtra("latitude", 0);
 
+             temperature = findViewById(R.id.temperature);
             String Url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&current_weather=true";
             response = createCall(Url);
             try {
-                response.body().string();
+                responseSplit = response.body().string().split(",");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
+            String[] splitResult = responseSplit[7].split(test);
+            String temp = splitResult[1];
+            String  test2 = "\"";
+            result = temp.replaceAll(test2, "");
+
+
+            temperature.setText(result+" Grad Celcius");
         }
     };
 
@@ -59,8 +76,12 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weatheroutput);
 
+        cityName = findViewById(R.id.cityname);
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("input");
+
+        cityName.setText(location);
 
         IntentFilter filter = new IntentFilter(LongAndAti);
         registerReceiver(longAndAtiReceiver, filter);
